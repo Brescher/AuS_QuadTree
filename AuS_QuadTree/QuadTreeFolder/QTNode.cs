04fe0c6db9d22e244e3b4442d7e81a6993942443
Brescher <73077282+Brescher@users.noreply.github.com>
2023-10-10 21:16:48 +0200

    atributy do qtnode

---
+++ AuS_QuadTree/QuadTreeFolder/QTNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AuS_QuadTree.QuadTreeFolder
+{
+    internal class QTNode<TKey> where TKey : IComparable<TKey>
+    {
+        private QTNode<TKey>[] children = new QTNode<TKey>[4];
+        private List<TKey> records;
+        private QTNode<TKey> parent;
+        private int x1, y1, x2, y2;
+        private bool isLeaf;
+
+        public QTNode()
+        {
+            isLeaf = true;
+        }
+    }
+}
---
+++ AuS_QuadTree/QuadTreeFolder/QuadTree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuS_QuadTree.QuadTreeFolder
+{
+    internal class QuadTree<TKey> where TKey : IComparable<TKey>
+    {
+    }
+}

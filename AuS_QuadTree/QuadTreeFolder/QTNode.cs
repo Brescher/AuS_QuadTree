3cb391c47735b784484b16bc953b5712ec0d570b
Brescher <73077282+Brescher@users.noreply.github.com>
2023-10-12 21:58:27 +0200

    vytvorenie synov node

--- AuS_QuadTree/QuadTreeFolder/QTNode.cs
+++ AuS_QuadTree/QuadTreeFolder/QTNode.cs
@@ -25,6 +25,32 @@ namespace AuS_QuadTree.QuadTreeFolder
             this.upperBoundY = upperY_;
         }
 
-        
+        public bool AllocateSons()
+        {
+            if (!HasSons())
+            {
+                children[0] = new QTNode<TKey>(lowerBoundX, lowerBoundY, upperBoundX/2d, upperBoundY/2d);
+                children[1] = new QTNode<TKey>(upperBoundX/2d, upperBoundY, upperBoundX, upperBoundY/2d);
+                children[2] = new QTNode<TKey>(upperBoundX/2d, upperBoundY/2d, upperBoundX, upperBoundY);
+                children[3] = new QTNode<TKey>(lowerBoundX, upperBoundY/2d, upperBoundX/2d, upperBoundY);
+                isLeaf = false;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public bool HasSons()
+        {
+            for(int i = 0; i < children.Length; i++)
+            {
+                if (children[i] == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
